ea5a81f3c096cd5ab7ef20b990a3d553d8188087
seantamturk <128218653+seantamturk@users.noreply.github.com>
2023-03-29 12:09:28 -0700

    wrote psuedo code

---
+++ script.js
@@ -0,0 +1,10 @@
+// make a function that will open a prompt box, ask "prompt will say "say R,P, or S"
+//we enter one of those
+// we save the return value from the prompt 
+// computer picks r, p , or s
+// we compare our return value to the computers value
+// if we win, tally a win, if we lose tally a loss, if it is a tie tally a tie
+// if we enter any other characters than R, P, or S, then we get an alert message that says "you must enter R, P, or S. It is case sensitive"
+    // it kicks us back to first prompt
+// when either you or the computer reaches 3 wins, an alert box ap[pears showing the wins for each player and who won. 
+// the box will give the option to play again by pressing okay
